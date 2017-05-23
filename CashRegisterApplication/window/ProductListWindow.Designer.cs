30569606197af8d6edd47bdcfe5ed06ccc8a0398
yorklin6 <yorklin6@gmail.com>
2017-05-23 08:46:10 +0800

    windows fill size

--- CashRegisterApplication/window/ProductListWindow.Designer.cs
+++ CashRegisterApplication/window/ProductListWindow.Designer.cs
@@ -49,9 +49,18 @@
             this.orderMsg = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.orderMoney = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.label7 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.panel5 = new System.Windows.Forms.Panel();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView_payWay)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView_productList)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView_order)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.panel5.SuspendLayout();
             this.SuspendLayout();
             // 
             // dataGridView_payWay
@@ -62,7 +71,8 @@
             this.dataGridView_payWay.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.Column8,
             this.Column9});
-            this.dataGridView_payWay.Location = new System.Drawing.Point(23, 320);
+            this.dataGridView_payWay.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView_payWay.Location = new System.Drawing.Point(0, 0);
             this.dataGridView_payWay.Name = "dataGridView_payWay";
             this.dataGridView_payWay.RowHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.Single;
             dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
@@ -76,7 +86,7 @@
             this.dataGridView_payWay.RowHeadersVisible = false;
             this.dataGridView_payWay.RowTemplate.Height = 23;
             this.dataGridView_payWay.ScrollBars = System.Windows.Forms.ScrollBars.None;
-            this.dataGridView_payWay.Size = new System.Drawing.Size(200, 99);
+            this.dataGridView_payWay.Size = new System.Drawing.Size(200, 168);
             this.dataGridView_payWay.TabIndex = 2;
             // 
             // Column8
@@ -111,14 +121,15 @@
             dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
             dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
             this.dataGridView_productList.DefaultCellStyle = dataGridViewCellStyle2;
+            this.dataGridView_productList.Dock = System.Windows.Forms.DockStyle.Fill;
             this.dataGridView_productList.EditMode = System.Windows.Forms.DataGridViewEditMode.EditOnEnter;
             this.dataGridView_productList.GridColor = System.Drawing.SystemColors.ControlDarkDark;
-            this.dataGridView_productList.Location = new System.Drawing.Point(-1, 12);
+            this.dataGridView_productList.Location = new System.Drawing.Point(0, 0);
             this.dataGridView_productList.MultiSelect = false;
             this.dataGridView_productList.Name = "dataGridView_productList";
             this.dataGridView_productList.RowHeadersVisible = false;
             this.dataGridView_productList.RowTemplate.Height = 23;
-            this.dataGridView_productList.Size = new System.Drawing.Size(1215, 297);
+            this.dataGridView_productList.Size = new System.Drawing.Size(800, 400);
             this.dataGridView_productList.TabIndex = 1;
             this.dataGridView_productList.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.productListDataGridView_CellContentClick);
             this.dataGridView_productList.CellEndEdit += new System.Windows.Forms.DataGridViewCellEventHandler(this.productListDataGridView_CellEndEdit);
@@ -193,16 +204,18 @@
             dataGridViewCellStyle3.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
             dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
             this.dataGridView_order.DefaultCellStyle = dataGridViewCellStyle3;
+            this.dataGridView_order.Dock = System.Windows.Forms.DockStyle.Fill;
             this.dataGridView_order.EditMode = System.Windows.Forms.DataGridViewEditMode.EditProgrammatically;
             this.dataGridView_order.GridColor = System.Drawing.SystemColors.ControlDarkDark;
-            this.dataGridView_order.Location = new System.Drawing.Point(260, 320);
+            this.dataGridView_order.Location = new System.Drawing.Point(0, 0);
             this.dataGridView_order.MultiSelect = false;
             this.dataGridView_order.Name = "dataGridView_order";
             this.dataGridView_order.ReadOnly = true;
             this.dataGridView_order.RowHeadersVisible = false;
             this.dataGridView_order.RowTemplate.Height = 23;
+            this.dataGridView_order.ScrollBars = System.Windows.Forms.ScrollBars.None;
             this.dataGridView_order.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.CellSelect;
-            this.dataGridView_order.Size = new System.Drawing.Size(204, 133);
+            this.dataGridView_order.Size = new System.Drawing.Size(200, 162);
             this.dataGridView_order.TabIndex = 5;
             this.dataGridView_order.CellEndEdit += new System.Windows.Forms.DataGridViewCellEventHandler(this.orderDataGridView_CellEndEdit);
             this.dataGridView_order.SelectionChanged += new System.EventHandler(this.orderDataGridView_SelectionChanged);
@@ -222,21 +235,67 @@
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(804, 349);
+            this.label7.Location = new System.Drawing.Point(93, 0);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(107, 84);
             this.label7.TabIndex = 38;
             this.label7.Text = "快捷键\r\nHOME:选择支付方式\r\nDELETE:删除商品\r\nF9:会员充值\r\nF4:挂单\r\nF5:挂单恢复\r\nEND:基本信息设置";
             // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.Controls.Add(this.dataGridView_productList);
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(800, 400);
+            this.panel1.TabIndex = 39;
+            // 
+            // panel2
+            // 
+            this.panel2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.panel2.Controls.Add(this.dataGridView_payWay);
+            this.panel2.Location = new System.Drawing.Point(0, 400);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(200, 168);
+            this.panel2.TabIndex = 40;
+            // 
+            // panel3
+            // 
+            this.panel3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel3.Controls.Add(this.label7);
+            this.panel3.Location = new System.Drawing.Point(584, 400);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(200, 168);
+            this.panel3.TabIndex = 41;
+            // 
+            // panel4
+            // 
+            this.panel4.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.panel4.Location = new System.Drawing.Point(399, 400);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(188, 165);
+            this.panel4.TabIndex = 42;
+            // 
+            // panel5
+            // 
+            this.panel5.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.panel5.Controls.Add(this.dataGridView_order);
+            this.panel5.Location = new System.Drawing.Point(200, 400);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(200, 162);
+            this.panel5.TabIndex = 43;
+            // 
             // ProductListWindow
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1212, 527);
-            this.Controls.Add(this.label7);
-            this.Controls.Add(this.dataGridView_order);
-            this.Controls.Add(this.dataGridView_productList);
-            this.Controls.Add(this.dataGridView_payWay);
+            this.ClientSize = new System.Drawing.Size(784, 562);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel5);
+            this.Controls.Add(this.panel4);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "ProductListWindow";
             this.Text = "收银台";
@@ -246,8 +305,12 @@
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView_payWay)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView_productList)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView_order)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.panel5.ResumeLayout(false);
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -269,5 +332,10 @@
         private System.Windows.Forms.DataGridViewTextBoxColumn ColumnMoney;
         private System.Windows.Forms.DataGridViewTextBoxColumn ColumnRemark;
         private System.Windows.Forms.DataGridViewTextBoxColumn ProductID;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Panel panel5;
     }
 }
